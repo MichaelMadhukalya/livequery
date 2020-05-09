40eb75a0de8a1f15bc4ddb5bef7890facad13203
michael.madhukalya@gmail.com <michael.madhukalya@gmail.com>
2020-05-08 22:32:53 -0400

    Adding tests for Json parsing logic

--- src/test/java/com/livequery/types/JsonObjectTest.java
+++ src/test/java/com/livequery/types/JsonObjectTest.java
@@ -1,11 +1,31 @@
 package com.livequery.types;
 
+import com.livequery.types.JsonType.UnCastableObjectToInstanceTypeException;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 
 public class JsonObjectTest {
     
     JsonObject jsonObject;
+    
+    /* Input data */
+    String INPUT_0 = "{}";
+    
+    String INPUT_1 = "{\"key\": \"value\"}";
+    
+    String INPUT_2 = "{\"key1\": \"value1\", \"key2\": \"value2\"}";
+    
+    String INPUT_3 = "{\"key1\": \"value1\", \"key2\": \"value2\", \"key3\": [1, 2, 3], \"key4\": "
+        + "{\"key5\": \"value5\", \"key6\": \"value6\"}}";
+    
+    String INPUT_4 = "{\"key1\": \"value1\", \"key2\": \"value2\", \"key3\": [1, [2, 3], 4], \"key4\": "
+        + "{\"key5\": \"value5\", \"key6\": {\"key7\": \"value7\", \"key8\" : \"value8\"}}}";
+    
+    String INPUT_5 = "{\"key1\": \"value1\", \"key2\": \"value2\", \"key3\": [1, 2, 3], \"key4\": "
+        + "{ \"key5\": \"value5\", \"key6\": \"value6\" }, \"key5\": [1, [2, [3, 4, 5]], {\"key6\": "
+        + "{\"key7\": [1, 2, {\"key8\": \"value8\", \"key9\": [1, 2, [[1,2,3], 4]]}]}}]}";
     
     @Before
     public void setUp() throws Exception {
@@ -13,5 +33,26 @@ public class JsonObjectTest {
     
     @After
     public void tearDown() throws Exception {
+    }
+    
+    @Test
+    public void emptyStringAsInput_Test() throws UnCastableObjectToInstanceTypeException {
+        jsonObject = JsonObject.newInstance();
+        jsonObject.cast(INPUT_0);
+        Assert.assertTrue(null != jsonObject.map && jsonObject.size() == 0);
+    }
+    
+    @Test
+    public void singleKeyAsInput_Test() throws UnCastableObjectToInstanceTypeException {
+        jsonObject = JsonObject.newInstance();
+        jsonObject.cast(INPUT_1);
+        Assert.assertTrue(null != jsonObject.map && jsonObject.size() == 1);
+    }
+    
+    @Test
+    public void multipleKeyAsInput_Test() throws UnCastableObjectToInstanceTypeException {
+        jsonObject = JsonObject.newInstance();
+        jsonObject.cast(INPUT_2);
+        Assert.assertTrue(null != jsonObject.map && jsonObject.size() == 2);
     }
 }
