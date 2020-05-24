b415032f39ba893cbe9c4a6790a5a48f09bc898d
michael.madhukalya@gmail.com <michael.madhukalya@gmail.com>
2020-05-24 16:20:40 -0400

    Add tests to verify parallel and concurrent Json parsing

--- src/test/java/com/livequery/types/JParserTest.java
+++ src/test/java/com/livequery/types/JParserTest.java
@@ -1,9 +1,31 @@
 package com.livequery.types;
 
+import com.livequery.types.JsonType.UnCastableObjectToInstanceTypeException;
+import java.util.Arrays;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import java.util.stream.Collectors;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 
 public class JParserTest {
+    String input_1 = "{\"key1\": \"value1\", \"key2\": \"value2\", \"key3\": [1, 2, 3], \"key4\": "
+        + "{ \"key5\": \"value5\", \"key6\": \"value6\"}, \"key7\": [1, [2, [3, 4, 5]], {\"key8\": "
+        + "{\"key9\": [0.0096, 8.0E+02, {\"key10\": \"value10\", \"key11\": [1001.01, 2.1E-03, [[1.6E+02,0.2,3.14159], 4.3E+03]]}]}}]}";
+    
+    String[] inputs = new String[]{
+        "{\"key\": \"value\"}",
+        "{\"key\": null}",
+        "{\"key\": 3.234}",
+        "{\"key\": false}",
+        "{\"key\": [1, 2.0, 3.14159]}"
+    };
     
     @Before
     public void setUp() throws Exception {
@@ -11,5 +33,60 @@ public class JParserTest {
     
     @After
     public void tearDown() throws Exception {
+    }
+    
+    @Test
+    public void verifyAsyncParserAccess_Test()
+        throws UnCastableObjectToInstanceTypeException, InterruptedException, ExecutionException, TimeoutException {
+        JsonObject jsonObject = JsonObject.newInstance();
+        jsonObject.cast(input_1);
+        
+        // Start an async parsing process in the background and wait for completion
+        CompletableFuture<?> completableFuture = CompletableFuture.runAsync(() -> createJsonObject());
+        completableFuture.get(1_000, TimeUnit.MILLISECONDS);
+        
+        Assert.assertTrue(null != jsonObject && jsonObject.size() == 5);
+    }
+    
+    private void createJsonObject() {
+        try {
+            JsonObject jsonObject = JsonObject.newInstance();
+            jsonObject.cast(input_1);
+            Assert.assertTrue(null != jsonObject && jsonObject.size() == 5);
+        } catch (UnCastableObjectToInstanceTypeException e) {
+        }
+    }
+    
+    @Test
+    public void verifyForkJoinPoolParserAccess_Test() {
+        // Verify parser access via ForkJoin pool (parallel stream). This test makes more sense when the number of CPUs is much
+        // smaller than the size of the "inputs" array since for Fork Join pool based on parallel streams the number of threads
+        // is typically set equal to the number of CPU cores available.
+        Arrays.asList(inputs).parallelStream().forEach(e -> createJsonObject(e));
+    }
+    
+    private void createJsonObject(String in) {
+        try {
+            JsonObject jsonObject = JsonObject.newInstance();
+            jsonObject.cast(in);
+            Assert.assertTrue(null != jsonObject && jsonObject.size() > 0);
+        } catch (UnCastableObjectToInstanceTypeException e) {
+        }
+    }
+    
+    @Test
+    public void verifyWorkerThreadPoolParserAccess_Test() throws InterruptedException, ExecutionException, TimeoutException {
+        // Create custom thread pool with two threads
+        ExecutorService service = Executors.newFixedThreadPool(2);
+        
+        // Use an array of completable futures to gather futures submitted to the above worker thread pool of size 2
+        CompletableFuture[] completableFutures = new CompletableFuture[5];
+        Arrays.asList(inputs).stream()
+            .map(e -> CompletableFuture.runAsync(() -> createJsonObject(e), service))
+            .collect(Collectors.toList())
+            .toArray(completableFutures);
+        
+        CompletableFuture completableFuture = CompletableFuture.allOf(completableFutures);
+        completableFuture.get(1_000, TimeUnit.MILLISECONDS);
     }
 }
